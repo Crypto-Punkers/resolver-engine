13442ccc4ac8824e9ea5415f01f020d970451360
Olaf Tomalka <olaf@tomalka.me>
2018-04-19 00:14:38 +0200

    Make the network example identity

--- examples/network.ts
+++ examples/network.ts
@@ -2,4 +2,6 @@ import { ResolverEngine, FsParser, UrlResolver } from "../src";
 
 const resolver = new ResolverEngine<string>({ debug: true }).addParser(new FsParser()).addResolver(new UrlResolver());
 
-resolver.require("https://pastebin.com/raw/D8ziKX0a").then(console.log);
+resolver
+  .require("https://raw.githubusercontent.com/ritave/resolver-engine/master/examples/network.ts")
+  .then(console.log);
