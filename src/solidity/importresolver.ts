d5d90c4bc303c1dc5e5cd51616a2084459a4441d
Krzysztof Adamek <squadack@o2.pl>
2018-12-12 11:24:09 -0500

    gatherSources sketch

--- src/solidity/importresolver.ts
+++ src/solidity/importresolver.ts
@@ -32,3 +32,37 @@ export function SolidityImportResolver() {
     .addResolver(UrlResolver())
     .addParser(ImportParser());
 }
+
+export function findImports<R extends ImportFile>(data: R) : string[] {
+  let result: string[] = [];
+  const regex: RegExp = /import \"([^\"]+)\";/g;
+  let match: RegExpExecArray | null;
+  while (match = regex.exec(data.source)) {
+    result.push(match[1]);
+    console.log(match);
+  }
+  return result;
+}
+
+export async function gatherSources<R extends ImportFile>(what: string, workingDir?: string, resolver?: ResolverEngine<R>): Promise<R[]> {
+//  resolver || (resolver = SolidityImportResolver());
+  let result: R[] = [];
+
+  if (!resolver) //TODO resolver should default to SolidityImportResolver()
+    throw Error("No resolver provided!");
+
+  let queue = [];
+
+  queue.push({cwd: workingDir, file: what});
+
+  while (queue.length > 0){
+    const fileData = queue.shift()!;
+    let tmp: R = await resolver.require(fileData.file, fileData.cwd);
+    const foundImports = findImports(tmp);
+    console.log(foundImports);
+    result.push(tmp);
+    //TODO add found imports to queue 
+  }
+
+  return result;
+}
