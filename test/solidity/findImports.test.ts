32da35a6e24e3d4bd726ec107ea3931150cfe33d
Krzysztof Adamek <squadack@o2.pl>
2018-12-13 10:58:46 -0500

    Added tests for single Solidity imports

--- test/solidity/findImports.test.ts
+++ test/solidity/findImports.test.ts
@@ -1,17 +1,47 @@
-// import { expect } from "chai";
-// import { findImports, ImportFile } from "../../src";
+import { expect } from "chai";
+import { findImports, ImportFile } from "../../src";
 
 describe("findImports", function() {
+    let providedFile: ImportFile = {path: "", source: ""};
+
     context("Double quotes", function() {
-        it("finds statements like: import \"file\";");
-        it("finds statements like: import \"file\" as _something_;");
-        it("finds statements like: import _something_ from \"file\";");
+        it("finds statements like: import \"file\";", async function() {
+            providedFile.source = 'pragma solidity ^0.5.0;\nimport "./SomeLib.sol";\n\ncontract SomeContract {}\n';
+            const result = findImports(providedFile);
+            expect(result).to.have.members(["./SomeLib.sol"]);
+        });
+
+        it("finds statements like: import \"file\" as _something_;", async function() {
+            providedFile.source = 'pragma solidity ^0.5.0;\nimport "./SomeLib.sol" as OtherLib;\n\ncontract SomeContract {}\n';
+            const result = findImports(providedFile);
+            expect(result).to.have.members(["./SomeLib.sol"]);
+        });
+
+        it("finds statements like: import _something_ from \"file\";", async function() {
+            providedFile.source = 'pragma solidity ^0.5.0;\nimport SomeLib from "./SomeLib.sol";\n\ncontract SomeContract {}\n';
+            const result = findImports(providedFile);
+            expect(result).to.have.members(["./SomeLib.sol"]);
+        });
     });
 
     context("Single quotes", function() {
-        it("finds statements like: import 'file';");
-        it("finds statements like: import 'file' as _something_;");
-        it("finds statements like: import _something_ from 'file';");
+        it("finds statements like: import 'file';", async function() {
+            providedFile.source = "pragma solidity ^0.5.0;\nimport './SomeLib.sol';\n\ncontract SomeContract {}\n";
+            const result = findImports(providedFile);
+            expect(result).to.have.members(["./SomeLib.sol"]);
+        });
+
+        it("finds statements like: import 'file' as _something_;", async function() {
+            providedFile.source = "pragma solidity ^0.5.0;\nimport './SomeLib.sol' as OtherLib;\n\ncontract SomeContract {}\n";
+            const result = findImports(providedFile);
+            expect(result).to.have.members(["./SomeLib.sol"]);
+        });
+
+        it("finds statements like: import _something_ from 'file';", async function() {
+            providedFile.source = "pragma solidity ^0.5.0;\nimport SomeLib from './SomeLib.sol';\n\ncontract SomeContract {}\n";
+            const result = findImports(providedFile);
+            expect(result).to.have.members(["./SomeLib.sol"]);
+        });
     });
 
     it("finds multiple import statements");
