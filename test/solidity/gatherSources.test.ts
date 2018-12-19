ca7afa7d3dad9d53d89a7044a0c252539427d39d
Krzysztof Adamek <squadack@o2.pl>
2018-12-19 03:12:07 -0500

    test added

--- test/solidity/gatherSources.test.ts
+++ test/solidity/gatherSources.test.ts
@@ -1,22 +1,41 @@
-// import { expect } from "chai";
-// import MockFs from "mock-fs";
+import { expect } from "chai";
+import MockFs from "mock-fs";
 // import * as path from "path";
-// import { ResolverEngine, SolidityImportResolver, ImportFile } from "../../src";
+import { gatherSources, ResolverEngine, SolidityImportResolver, ImportFile } from "../../src";
 
 describe("gatherSources function", function() {
-    // let resolver: ResolverEngine<ImportFile>;
+    let resolver: ResolverEngine<ImportFile> = SolidityImportResolver();
 
-    // before(function() {
-    //     resolver = SolidityImportResolver();
-    // });
-
-    //  afterEach(function() {
-    //     MockFs.restore();
-    // });
+     afterEach(function() {
+        MockFs.restore();
+    });
     
-    it("gathers files included by given file");
-    it("gathers all files required to compile")
+    it("gathers files included by given file", async function() {
+        MockFs({
+            "mainfile.sol": 'blahblah;\nimport "./otherfile.sol";\nimport "./somethingelse.sol";\nrestoffileblahblah',
+            "otherfile.sol": "otherfilecontents",
+            "somethingelse.sol": "somethingelsecontents"
+        });
+        const fileList = await gatherSources("mainfile.sol", process.cwd(), resolver);
+        expect(fileList).to.have.deep.members([
+            {
+                path: process.cwd() + '/mainfile.sol',
+                source: 'blahblah;\nimport "./otherfile.sol";\nimport "./somethingelse.sol";\nrestoffileblahblah'
+            },
+            {
+                path: process.cwd() + '/otherfile.sol',
+                source: 'otherfilecontents'
+            },
+            {
+                path: process.cwd() + '/somethingelse.sol',
+                source: 'somethingelsecontents'
+            }
+        ]);
+    });
+
+    it("gathers all files required to compile");
     it("does not include the same file twice");
+    it("works without passing resolver to it");
 
     
 });
