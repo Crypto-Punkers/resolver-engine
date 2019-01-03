c8e820fbe4e2f54ef005e51b572fc86134d94296
Krzysztof Adamek <squadack@o2.pl>
2019-01-03 13:31:25 -0500

    NodeResolver now utilises cwd from ctx

--- src/resolvers/noderesolver.ts
+++ src/resolvers/noderesolver.ts
@@ -1,12 +1,21 @@
 import Debug from "debug";
-import { SubResolver } from ".";
+import path from "path";
+import { SubResolver, ResolverContext } from ".";
 
 const debug = Debug("resolverengine:noderesolver");
 
 export function NodeResolver(): SubResolver {
-  return async (what: string): Promise<string | null> => {
+  return async (what: string, ctx: ResolverContext): Promise<string | null> => {
     try {
-      return require.resolve(what, {paths: [process.cwd()]});
+      let requirePaths: string[] = [];
+      let currentPath = ctx.cwd;
+      let lastPath: string = "";
+      do {
+        requirePaths.push(currentPath);
+        lastPath = currentPath;
+        currentPath = path.dirname(currentPath);
+      } while (currentPath !== lastPath);
+      return require.resolve(what, {paths: requirePaths});
     } catch (e) {
       debug("Node's require.resolve failed, returning null", e);
       return null;
