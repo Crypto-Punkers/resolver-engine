654296ac09e8ab68af5554322314c625debf37ab
Radosław Cymer <cymerrad@gmail.com>
2019-01-07 18:25:38 +0100

    Uncomment & tweak for single file testing

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   // Note: '*.test.ts' intentionally omitted; '*.spec.ts' stands out in VSCode as it is yellow
   // Also, no tsx files.
   testRegex: "(/__tests__/.*|(\\.|/)(spec))\\.ts$",
+  // testRegex: "__tests__/.*gatherSources.*$",
   moduleFileExtensions: ["js", "jsx", "ts"],
   globals: {
     "ts-jest": {
