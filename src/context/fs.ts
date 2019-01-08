7ef74a39c0ca47025275e746a71ac28f7987af3e
Radosław Cymer <cymerrad@gmail.com>
2019-01-08 18:35:38 +0100

    It would be nice to prove to myself that something in JS just "can't be done"

--- src/context/fs.ts
+++ src/context/fs.ts
@@ -1,5 +1,6 @@
 import { PathLike, Stats, WriteStream } from "fs";
-import { fs as memfs } from "memfs";
+import { fs as memfs, vol } from "memfs";
+import { TEncoding } from "memfs/lib/encoding";
 import { IError, IReadFileOptions, TCallback } from "memfs/lib/volume";
 
 // TODO
@@ -28,13 +29,30 @@ export interface FSWrapper {
         },
   ): WriteStream;
   stat(path: PathLike, callback: (err: NodeJS.ErrnoException | undefined, stats: Stats) => void): void;
+  open(
+    path: PathLike,
+    flags: string | number,
+    mode: string | number | undefined | null,
+    callback: (err: NodeJS.ErrnoException | undefined, fd: number) => void,
+  ): void;
+  write(
+    fd: number,
+    string: any,
+    position: number | undefined | null,
+    encoding: string | undefined | null,
+    callback: (err: NodeJS.ErrnoException | undefined, written: number, str: string) => void,
+  ): void;
 }
 
-// I'm getting weird errors, so I will superimpose some extra type safety and error handling on memfs
+// I'm getting weird errors, so I will superimpose some extra type safety and error handling on memfs.
+// All NodeJS.ErrnoException will also get a '| undefined' which I think is more suggestive to check for errors.
 export class MemFSWrapped implements FSWrapper {
   private fs: typeof memfs;
   constructor() {
     this.fs = memfs;
+
+    // define /tmp/ dir
+    vol.fromJSON({ ".stub": "" }, "/tmp/");
   }
 
   writeFile(
@@ -94,6 +112,42 @@ export class MemFSWrapped implements FSWrapper {
     // shorthand for
     let callbackRepackaged = callbackRepackagedShorthand(callback);
     return this.fs.stat(path, callbackRepackaged);
+  }
+  open(
+    path: PathLike,
+    flags: string | number,
+    mode: string | number | null | undefined,
+    callback: (err: NodeJS.ErrnoException | undefined, fd: number) => void,
+  ): void {
+    let callbackRepackaged = callbackRepackagedShorthand(callback);
+    return this.fs.open(path, flags, callbackRepackaged);
+  }
+  write(
+    fd: number,
+    string: any,
+    position: number | null | undefined,
+    encoding: string | null | undefined,
+    callback: (err: NodeJS.ErrnoException | undefined, written: number, str: string) => void,
+  ): void {
+    // this one is a very special snowflake
+    // it's really funny how even memfs author gave up on this one
+    let callbackRepack: (...args: any[]) => void = (err, written, data) => {
+      if (typeof data === "undefined") {
+        if (!err) {
+          // split to keep compiler happy
+          // only case not handled by the original callback: no error, undefined data
+          // => still call out an error
+          return callback(new Error("undefined data"), written, "");
+        } else {
+          return callback(err, written, "");
+        }
+      } else {
+        return callback(err, written, data);
+      }
+    };
+    let positionRepack = !position ? 0 : position;
+    let encodingRepack = !encoding ? "utf8" : (encoding as TEncoding);
+    return this.fs.write(fd, string, positionRepack, encodingRepack, callbackRepack);
   }
 }
 
--- test/utils.ts
+++ test/utils.ts
@@ -1,6 +1,6 @@
-import { BrowserService, env_t, IResolverServiceLayer, NodeService, ResolverContext } from "../src";
+import { BrowserService, env_t, IResolverServiceLayer, ResolverContext, TestService } from "../src";
 
-export function defaultContext(environment: env_t): ResolverContext {
+export function defaultContext(environment?: env_t): ResolverContext {
   return {
     cwd: process.cwd(),
     environment: environment,
@@ -8,10 +8,10 @@ export function defaultContext(environment: env_t): ResolverContext {
   } as ResolverContext;
 }
 
-function getSystem(env: env_t): IResolverServiceLayer {
+function getSystem(env?: env_t): IResolverServiceLayer {
   if (env === "browser") {
     return new BrowserService();
   } else {
-    return new NodeService("test");
+    return new TestService();
   }
 }
