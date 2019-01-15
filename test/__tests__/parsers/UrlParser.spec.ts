f646d96a3ea37800ee97af74fa16bc818b9807cc
Radosław Cymer <cymerrad@gmail.com>
2019-01-15 18:42:33 +0100

    Filename correction

---
+++ test/__tests__/parsers/UrlParser.spec.ts
@@ -0,0 +1,35 @@
+import nock from "nock";
+import { UrlParser } from "../../../src/parsers/urlparser";
+
+// input, output
+const incorrectUrls = [["", null], ["www.google.com", null]];
+const correctUrls = [["http://example.com/some/file.txt", "http://example.com/some/file.txt"]];
+const data = incorrectUrls.concat(correctUrls);
+
+describe(UrlParser, () => {
+  const parser = UrlParser(); // unintuitive af
+
+  beforeAll(() => {
+    nock.disableNetConnect();
+    correctUrls.forEach(pair => {
+      const [url, content] = pair;
+
+      if (content) {
+        nock(url)
+          .get("")
+          .reply(200, content)
+          .persist();
+      } else {
+        nock(url)
+          .get("")
+          .reply(404, "")
+          .persist();
+      }
+    });
+  });
+
+  it.each(data)("should parse %o into %o", async (input, expected) => {
+    const output = await parser(input);
+    expect(output).toBe(expected);
+  });
+});
