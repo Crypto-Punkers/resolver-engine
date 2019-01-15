106f157c0eb8207ec2f6ee09d616f7babf3c0287
Radosław Cymer <cymerrad@gmail.com>
2019-01-15 18:11:24 +0100

    Updated test cases for Github import

--- test/__tests__/resolvers/GitHubResolver.spec.ts
+++ test/__tests__/resolvers/GitHubResolver.spec.ts
@@ -3,10 +3,6 @@ import { GithubResolver } from "../../../src/resolvers";
 const data = [
   ["", null],
   [
-    "https://github.com/Crypto-Punkers/resolver-engine/examples/github.ts",
-    "https://api.github.com/repos/Crypto-Punkers/resolver-engine/contents/examples/github.ts",
-  ],
-  [
     "https://github.com/Crypto-Punkers/resolver-engine/blob/master/examples/github.ts",
     "https://raw.githubusercontent.com/Crypto-Punkers/resolver-engine/master/examples/github.ts",
   ],
@@ -14,9 +10,17 @@ const data = [
     "https://github.com/Crypto-Punkers/resolver-engine/blob/cymerrad/remix-ide-integration/examples/github.ts",
     "https://raw.githubusercontent.com/Crypto-Punkers/resolver-engine/cymerrad/remix-ide-integration/examples/github.ts",
   ],
+  [
+    "github:Crypto-Punkers/resolver-engine/examples/github.ts",
+    "https://raw.githubusercontent.com/Crypto-Punkers/resolver-engine/master/examples/github.ts",
+  ],
+  [
+    "https://github.com/Crypto-Punkers/resolver-engine/examples/github.ts",
+    "https://raw.githubusercontent.com/Crypto-Punkers/resolver-engine/master/examples/github.ts",
+  ],
 ];
 
-describe("GitHubResolver", () => {
+describe.only("GitHubResolver", () => {
   const subject = GithubResolver();
 
   it.each(data)("testing %o", async (input, output) => {
