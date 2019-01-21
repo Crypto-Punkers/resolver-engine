c1b72643a3435312595bb1e873a7197cc8382d87
Radosław Cymer <cymerrad@gmail.com>
2019-01-21 16:45:37 +0100

    Missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { BrowserImportResolver, SolidityImportResolver } from "./solidity";
 
 const defaultResolver = SolidityImportResolver();
+const browserResolver = BrowserImportResolver();
 
 export { SolidityImportResolver, BrowserImportResolver };
+export { browserResolver as browser, defaultResolver as default };
 
-export default defaultResolver;
+// so it seems like default exports cause problems all around
+// one of the voices against them, voice that we know personally:
+// https://blog.neufund.org/why-we-have-banned-default-exports-and-you-should-do-the-same-d51fdc2cf2ad
--- src/solidity/importresolver.ts
+++ src/solidity/importresolver.ts
@@ -1,7 +1,7 @@
 import { FsParser } from "../parsers";
 import { UrlParser } from "../parsers/urlparser";
 import { Options, ResolverEngine } from "../resolverengine";
-import { FsResolver, GithubResolver, NodeResolver, UriResolver } from "../resolvers";
+import { FsResolver, GithubResolver, IPFSResolver, NodeResolver, SwarmResolver, UriResolver } from "../resolvers";
 import { EthPmResolver } from "./ethpmresolver";
 import { ImportFile, ImportParser } from "./importparser";
 
@@ -19,5 +19,7 @@ export function BrowserImportResolver(options?: Options) {
   return new ResolverEngine<ImportFile>(options)
     .addResolver(GithubResolver())
     .addResolver(UriResolver())
+    .addResolver(IPFSResolver())
+    .addResolver(SwarmResolver())
     .addParser(ImportParser([UrlParser()]));
 }
