d3bdb7defffe6b0ffc0cdecc24ff0e3d7d08099d
Olaf Tomalka <olaf.tomalka@gmail.com>
2019-01-25 12:40:51 +0100

    Feedback after code review

--- src/solidity/utils.ts
+++ src/solidity/utils.ts
@@ -26,17 +26,31 @@ export function findImports(data: ImportFile): string[] {
 
 interface ImportTreeNode extends ImportFile {
   uri: string;
+  // uri and url the same as in rest of resolver-engine
+  // it might mean github:user/repo/path.sol and raw link
+  // or it might mean relative vs absolute file path
   imports: { uri: string; url: string }[];
 }
 
-async function traverseSources(
-  whats: string[],
+/**
+ * This function accepts root files to be searched for and resolves the sources, finds the imports in each source and traverses the whole dependency tree gathering absolute and uri paths
+ * @param roots
+ * @param workingDir
+ * @param resolver
+ */
+async function gatherDepenencyTree(
+  roots: string[],
   workingDir: string,
   resolver: ResolverEngine<ImportFile>,
 ): Promise<ImportTreeNode[]> {
   let result: ImportTreeNode[] = [];
   let alreadyImported = new Set();
 
+  /**
+   * This function traverses the depedency tree and calculates absolute paths for each import on the way storing each file in in a global array
+   * @param file File in a depedency that should now be traversed
+   * @returns An absolute path for the requested file
+   */
   async function dfs(file: { searchCwd: string; uri: string }): Promise<string> {
     const url = await resolver.resolve(file.uri, file.searchCwd);
     if (alreadyImported.has(url)) {
@@ -61,28 +75,49 @@ async function traverseSources(
     return resolvedFile.url;
   }
 
-  await Promise.all(whats.map(what => dfs({ searchCwd: workingDir, uri: what })));
+  await Promise.all(roots.map(what => dfs({ searchCwd: workingDir, uri: what })));
 
   return result;
 }
 
-export async function gatherSources(
-  whats: string[],
-  workingDir: string,
-  resolver: ResolverEngine<ImportFile> = SolidityImportResolver(),
-): Promise<ImportFile[]> {
-  return (await traverseSources(whats, workingDir, resolver)).map(node => {
+function stripNodes(nodes: ImportTreeNode[]): ImportFile[] {
+  return nodes.map(node => {
     return { url: node.url, source: node.source };
   });
 }
 
-export async function gatherSourcesAndCanonizeImports(
-  whats: string[],
+/**
+ * Starts with roots and traverses the whole depedency tree of imports, returning an array of sources
+ * @param roots
+ * @param workingDir What's the starting working dir for resolving relative imports in roots
+ * @param resolver
+ */
+export async function gatherSources(
+  roots: string[],
   workingDir: string,
   resolver: ResolverEngine<ImportFile> = SolidityImportResolver(),
-) {
+): Promise<ImportFile[]> {
+  return stripNodes(await gatherDepenencyTree(roots, workingDir, resolver));
+}
+
+/**
+ * This function gathers sources and **REWRITES IMPORTS** inside the source files into resolved, absolute paths instead of using shortcut forms
+ * Because the remapping api in solc is not compatible with multiple existing projects and frameworks, changing relative paths to absolute paths
+ * makes us avoid any need for finding imports after starting the solc compilation
+ * @param roots
+ * @param workingDir What's the starting working dir for resolving relative imports in roots
+ * @param resolver
+ */
+export async function gatherSourcesAndCanonizeImports(
+  roots: string[],
+  workingDir: string,
+  resolver: ResolverEngine<ImportFile> = SolidityImportResolver(),
+): Promise<ImportFile[]> {
   function canonizeFile(file: ImportTreeNode) {
     file.imports.forEach(i => (file.source = file.source.replace(i.uri, i.url)));
   }
-  return (await traverseSources(whats, workingDir, resolver)).map(canonizeFile);
+
+  let sources = await gatherDepenencyTree(roots, workingDir, resolver);
+  sources.forEach(canonizeFile);
+  return stripNodes(sources);
 }
