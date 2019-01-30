623b1a58d6c1b73220775dc5a3caa1a61f757727
Radosław Cymer <cymerrad@gmail.com>
2019-01-30 18:21:31 +0100

    Building in guaranteed order (workaround for now)

--- packages/imports-fs/src/index.ts
+++ packages/imports-fs/src/index.ts
@@ -1,6 +1,15 @@
 export { ImportsFsEngine } from "./importsfsengine";
-import { EthPmResolver } from "./resolvers/ethpmresolver";
+import { parsers as fs_p, resolvers as fs_r } from "@resolver-engine/fs";
+import { parsers as imp_p, resolvers as imp_r } from "@resolver-engine/imports";
+import { EthPmResolver } from "./resolvers";
 
 export const resolvers = {
   EthPmResolver,
+  ...fs_r,
+  ...imp_r,
+};
+
+export const parsers = {
+  ...fs_p,
+  ...imp_p,
 };
