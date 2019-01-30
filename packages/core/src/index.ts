c8de6e9946997eef5e203a0019dc1056d4cfb46f
Radosław Cymer <cymerrad@gmail.com>
2019-01-30 18:15:57 +0100

    Explanation on why do it that way

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,9 @@
 import { UrlParser } from "./parsers/index";
 import { UriResolver } from "./resolvers/index";
+
+// exporting interfaces, types and classes
+// packing all the resolvers and parserse inside namespaces
+
 export { SubParser } from "./parsers/index";
 export * from "./resolverengine";
 export { SubResolver } from "./resolvers/index";
