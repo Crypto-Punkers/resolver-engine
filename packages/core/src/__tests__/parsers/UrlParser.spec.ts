1e79002a8764eb8e1ae862dd139d46ee11bda986
Radosław Cymer <cymerrad@gmail.com>
2019-02-18 16:07:05 +0100

    Idiomatic Jest tests in every package (duplication)

---
+++ packages/core/src/__tests__/parsers/UrlParser.spec.ts
@@ -0,0 +1,34 @@
+import nock from "nock";
+import { parsers } from "../..";
+
+const incorrectUrls = [["", null], ["www.google.com", null]];
+const correctUrls = [["http://example.com/some/file.txt", "http://example.com/some/file.txt"]];
+const data = incorrectUrls.concat(correctUrls);
+
+describe("UrlParser", () => {
+  const parser = parsers.UrlParser();
+
+  beforeAll(() => {
+    nock.disableNetConnect();
+    correctUrls.forEach(pair => {
+      const [url, content] = pair;
+
+      if (content) {
+        nock(url)
+          .get("")
+          .reply(200, content)
+          .persist();
+      } else {
+        nock(url)
+          .get("")
+          .reply(404, "")
+          .persist();
+      }
+    });
+  });
+
+  it.each(data)("should parse %o into %o", async (input, expected) => {
+    const output = await parser(input, { resolver: "doesn't matter" });
+    expect(output).toBe(expected);
+  });
+});
