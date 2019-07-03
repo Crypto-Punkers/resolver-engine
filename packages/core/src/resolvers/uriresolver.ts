76173e2efdd4d6c96d39b1126592fd61fc7e5f84
Santiago Palladino <spalladino@gmail.com>
2019-07-03 11:03:16 -0300

    Implement suggestions by @ritave

--- packages/core/src/resolvers/uriresolver.ts
+++ packages/core/src/resolvers/uriresolver.ts
@@ -3,7 +3,6 @@ import { Context } from "..";
 import { SubResolver } from "./subresolver";
 
 export function UriResolver(): SubResolver {
-  // TODO: make a resolver that actually checks if something is a valid http link
   return async function http(uri: string, ctx: Context): Promise<string | null> {
     if (!isUrl(uri)) {
       return null;
